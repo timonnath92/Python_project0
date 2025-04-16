e903ca184f31b8bea84732f08364cc59be3855e9
timonnath92 <timonnath92@gmail.com>
2025-04-16 19:39:02 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter 1st number: "))
+b=int(input("Enter 2nd number: "))
+print("Addition: ",a+b,"\nSubstraction: ",
+      a-b,"\nMultiplication: ",a*b,"\nDivison: ",
+      a/b
+)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+a=str(input("Enter for First name: "))
+b=str(input("Enter for Last name: "))
+c="!"
+print("Hello",a,b+c,"Welcome to the Python program")
